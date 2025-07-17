d333b0d7c54a5ca0f111382ee9aa491fd07c7039
Gwilain <ghislain.gillet@gmail.com>
2025-07-17 17:26:54 +0200

    correction appel  js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
-import { initColorTheme } from './resources/js/colorTheme.js';
-import { initLayout } from './resources/js/layout.js';
+import { initColorTheme } from './colorTheme.js';
+import { initLayout } from './layout.js';
 
 document.addEventListener('DOMContentLoaded', init);
 
